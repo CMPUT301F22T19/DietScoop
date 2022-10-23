3ac79754a73a05b668e9138a8c496b33efdfcbed
Gurbir Sandha <77547332+gurbirsandha@users.noreply.github.com>
2022-10-23 16:43:31 -0600

    started on Database wrapper class

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Database.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Database.java
@@ -1,7 +1,13 @@
 package com.example.dietscoop;
 
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.EventListener;
@@ -10,6 +16,46 @@ import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Database {
+    private static final String TAG = "testing";
     FirebaseFirestore db;
+    CollectionReference ingredientStorage;
+    CollectionReference recipes;
+    CollectionReference ingredientsInRecipe;
+
+    public Database() {
+        db = FirebaseFirestore.getInstance();
+        ingredientStorage = db.collection("IngredientStorage");
+
+    }
+
+    public void addIngredientToStorage(Ingredient ingredient) {
+        Map<String, Object> data1 = new HashMap<>();
+
+        data1.put("amount", Integer.valueOf(ingredient.getAmount());
+        data1.put("unit", ingredient.getMeasurementUnit());
+
+
+        ingredientStorage.document(ingredient.getDescription()).set(data1);
+    }
+    public void getIngredientList() {
+        ingredientStorage
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                Log.d(TAG, document.getId() + " => " + document.getData());
+                            }
+                        } else {
+                            Log.d(TAG, "Error getting documents: ", task.getException());
+                        }
+                    }
+                });
+
+    }
 }
--- DietScoopApp/app/src/main/java/com/example/dietscoop/MainActivity.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/MainActivity.java
@@ -10,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Database db = new Database();
+        db.addIngredientToStorage();
+        db.getIngredientList();
     }
 }
