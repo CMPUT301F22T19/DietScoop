6039d1ec0443b90e94c02adf2c0542d63cbcdeda
Gurbir Sandha <77547332+gurbirsandha@users.noreply.github.com>
2022-10-23 13:14:24 -0600

    fixed prefer_project setting

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Database.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Database.java
@@ -1,4 +1,15 @@
 package com.example.dietscoop;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
 public class Database {
+    FirebaseFirestore db;
 }
