18341ef847db4518d6bd48f65faa592af42a3336
marcos_gonzalezmayedo <magonzal@ualberta.ca>
2022-10-25 23:03:01 -0600

    Added IngredientTest and IngredientInStorageTest

---
+++ DietScoopApp/app/src/test/java/com/example/dietscoop/FoodStorageTest.java
@@ -0,0 +1,25 @@
+package com.example.dietscoop;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * This Test oversees the 'local' functionality of the methods
+ * within the FoodStorage Class.
+ *
+ *TODO: Add potential interactivity with Database Object.
+ */
+public class FoodStorageTest {
+
+    private FoodStorage sampleFoodStorage;
+
+    public FoodStorage sampleFoodStorage() {
+        return new FoodStorage();
+    }
+
+    /**
+     * TODO: Add the implementation for the Database.
+     */
+
+}
