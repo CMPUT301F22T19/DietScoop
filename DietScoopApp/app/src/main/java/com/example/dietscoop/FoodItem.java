af30e6fdc973eeae9a5fcbb4e71e43fc47614282
Arjun <arjun.mehta1001@gmail.com>
2022-11-03 23:38:56 -0600

    FoodItem and Category enum added

--- DietScoopApp/app/src/main/java/com/example/dietscoop/FoodItem.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/FoodItem.java
@@ -3,12 +3,24 @@ package com.example.dietscoop;
 import java.io.Serializable;
 
 public abstract class FoodItem implements Serializable {
+/**
+ * The abstract class FoodItem abstracts the shared methods of ingredient and recipe
+ */
+public abstract class FoodItem {
     protected String description;
 
+    /**
+     * Getter method for FoodItem description
+     * @return description of food item
+     */
     public String getDescription() {
         return description;
     }
 
+    /**
+     * Setter method for FoodItem description
+     * @param description the new description
+     */
     public void setDescription(String description) {
         this.description = description;
     }
