86f29edfd1bb6285ae3cd888ede26dcd9cbb161c
Arjun <arjun.mehta1001@gmail.com>
2022-11-04 10:37:36 -0600

    documeting of ingredient comparator and ingredient in recipe completed

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Ingredient.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Ingredient.java
@@ -65,20 +65,42 @@ public abstract class Ingredient extends FoodItem implements Serializable {
         return amount;
     }
 
+    /**
+     * setter for ingredient amount
+     * @param amount value to set
+     */
     public void setAmount(double amount) {
         this.amount = amount;
     }
 
+    /**
+     * getter for ingredient measurement unit
+     * @return measurement unit of ingredient
+     */
     public String getMeasurementUnit() {
         return measurementUnit;
     }
 
+    /**
+     * setter for ingredient measurement unit
+     * @param measurementUnit value to set.
+     */
     public void setMeasurementUnit(String measurementUnit) {
         this.measurementUnit = measurementUnit;
     }
+
+    /**
+     * getter for ingredient category
+     * @return ingredient category
+     */
     public Category getCategory() {
         return category;
     }
+
+    /**
+     * getter for ingredient category name
+     * @return Stringified ingredient category
+     */
     public String getCategoryName() {return category.name(); }
 
 }
