dbfb30611b5c71cf189f5227eb5ceed9ff071172
virkSehbazz12 <71534099+virkSehbazz12@users.noreply.github.com>
2022-11-11 14:58:39 -0700

    implemented clickable text views for ingredient list sorting

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Activities/IngredientListActivity.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Activities/IngredientListActivity.java
@@ -1,25 +1,23 @@
 package com.example.dietscoop.Activities;
 
 
-import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.graphics.Color;
-import android.os.Build;
 import android.os.Bundle;
 
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.dietscoop.Fragments.IngredientAddFragment;
 import com.example.dietscoop.Data.Ingredient.IngredientInStorage;
 import com.example.dietscoop.Database.IngredientStorage;
 import com.example.dietscoop.Adapters.IngredientStorageAdapter;
 import com.example.dietscoop.R;
-import com.example.dietscoop.Fragments.sortIngredientByFragment;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
@@ -27,7 +25,7 @@ import java.util.ArrayList;
 /**
  * Class tying to Ingredient list. Associated with the activity_ingredient_list.xml layout.
  */
-public class IngredientListActivity extends AppCompatActivity implements IngredientAddFragment.OnFragmentInteractionListener, sortIngredientByFragment.OnFragmentInteractionListener, RecyclerItemClickListener {
+public class IngredientListActivity extends AppCompatActivity implements IngredientAddFragment.OnFragmentInteractionListener, RecyclerItemClickListener {
 
     IngredientStorage foodStorage;
     IngredientStorageAdapter ingredientStorageAdapter;
@@ -37,7 +35,14 @@ public class IngredientListActivity extends AppCompatActivity implements Ingredi
     Button recipesButton;
     Button mealsButton;
     Button shoppingButton;
-    Button sortButton;
+
+    TextView nameSort, categorySort, bestBeforeSort, locationSort;
+    public enum sortSelection {
+        NAME,
+        CATEGORY,
+        DATE,
+        LOCATION
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,31 +60,15 @@ public class IngredientListActivity extends AppCompatActivity implements Ingredi
         foodStorage.setupIngredientSnapshotListener(ingredientStorageAdapter);
         foodStorage.getIngredientStorageFromDatabase();
 
-        //testing code:
-//        //IngredientStorage sampleIngredientStorage = new IngredientStorage();
-//        IngredientInStorage sampleIngredient = new IngredientInStorage("Poop", "kg",
-//                5, 2022, 4, 25, Location.freezer, Category.meat);
-//        //sampleIngredientStorage.setupIngredientSnapshotListener(); //TODO: need to add the pass value.
-//        //sampleIngredientStorage.addIngredientToStorage(sampleIngredient);
-//        foodStorage.addIngredientToStorage(sampleIngredient);
-//
-//        sampleIngredient = new IngredientInStorage("Roop", "kg",
-//                5, 2022, 5, 25, Location.fridge, Category.meat);
-//        foodStorage.addIngredientToStorage(sampleIngredient);
-//
-//        sampleIngredient = new IngredientInStorage("Toop", "kg",
-//                5, 2022, 6, 25, Location.fridge, Category.fruit);
-//        foodStorage.addIngredientToStorage(sampleIngredient);
-//
-//        sampleIngredient = new IngredientInStorage("Scoop", "kg",
-//                5, 2022, 6, 25, Location.pantry, Category.vegetable);
-//        foodStorage.addIngredientToStorage(sampleIngredient);
-
         ingredientButton = findViewById(R.id.ingr_nav);
         recipesButton = findViewById(R.id.recipes_nav);
         mealsButton = findViewById(R.id.meals_nav);
         shoppingButton = findViewById(R.id.shopping_nav);
-        sortButton = findViewById((R.id.sort_button));
+
+        nameSort = findViewById(R.id.name_sort);
+        categorySort = findViewById(R.id.category_sort);
+        bestBeforeSort = findViewById(R.id.bestbefore_sort);
+        locationSort = findViewById(R.id.location_sort);
 
         ingredientButton.setBackgroundColor(Color.rgb(252, 186, 3));
 
@@ -87,10 +76,35 @@ public class IngredientListActivity extends AppCompatActivity implements Ingredi
         addIngredientButton.setOnClickListener((v) -> new IngredientAddFragment().show(getSupportFragmentManager(), "ADD_INGREDIENT"));
 
         recipesButton.setOnClickListener(unused -> switchToRecipes());
-
-        sortButton.setOnClickListener(view -> new sortIngredientByFragment().show(getFragmentManager(),"SORT_BY"));
-
         ingredientStorageAdapter.setItemClickListener(this);
+
+        nameSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSortSelection(sortSelection.NAME);
+            }
+        });
+
+        categorySort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSortSelection(sortSelection.CATEGORY);
+            }
+        });
+
+        bestBeforeSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSortSelection(sortSelection.DATE);
+            }
+        });
+
+        locationSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onSortSelection(sortSelection.LOCATION);
+            }
+        });
 
     }
 
@@ -106,7 +120,12 @@ public class IngredientListActivity extends AppCompatActivity implements Ingredi
         addIngredientButton.setOnClickListener((e) -> new IngredientAddFragment().show(getSupportFragmentManager(), "ADD_INGREDIENT"));
 
     }
-    
+
+    public void onSortSelection(sortSelection sortBy) {
+        foodStorage.sortBy(sortBy);
+        ingredientStorageAdapter.notifyDataSetChanged();
+    }
+
     @Override
     public void onOkPressed(IngredientInStorage newIngredientInStorage) {
         foodStorage.addIngredientToStorage(newIngredientInStorage);
@@ -123,24 +142,11 @@ public class IngredientListActivity extends AppCompatActivity implements Ingredi
     }
 
     @Override
-    public void onSortSelection(sortIngredientByFragment.selection sortBy) {
-        foodStorage.sortBy(sortBy);
-        ingredientStorageAdapter.notifyDataSetChanged();
-    }
-
-    @Override
     public void onItemClick(View view, int position) {
 
         ArrayList<IngredientInStorage> ingredients = foodStorage.getIngredientStorage();
         IngredientInStorage ingredient = ingredients.get(position);
         new IngredientAddFragment(ingredient).show(getSupportFragmentManager(), "EDIT_INGREDIENT");
     }
-
-    // POSSIBLE TODO: could be used to fix animations
-//    @Override
-//    public void finish() {
-//        super.finish();
-//        overridePendingTransition(androidx.navigation.ui.R.anim.nav_default_exit_anim, androidx.navigation.ui.R.anim.nav_default_enter_anim);
-//    }
 
 }
