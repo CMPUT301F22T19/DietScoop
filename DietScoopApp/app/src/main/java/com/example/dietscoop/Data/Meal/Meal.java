e6268d1fa3c9d8e44b0f2f91a4712d1028ae74d9
marcos_gonzalezmayedo <magonzal@ualberta.ca>
2022-11-15 10:28:43 -0700

    Added base of meal container and meal enums.

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Data/Meal/Meal.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Data/Meal/Meal.java
@@ -1,12 +1,14 @@
 package com.example.dietscoop.Data.Meal;
 
+import com.example.dietscoop.Data.Ingredient.IngredientCategory;
 import com.example.dietscoop.Data.Ingredient.IngredientInStorage;
+import com.example.dietscoop.Data.Ingredient.Location;
 import com.example.dietscoop.Data.Recipe.Recipe;
 
 import java.util.ArrayList;
 
 /**
- * Class that works as a wrapper for meals in a day:
+ * Class that works as a container for meals in a day:
  *
  *  1. Contains recipes and ingredients for the given meal.
  *  2. Has optional time that shows when to eat the meal.
@@ -34,6 +36,46 @@ public class Meal {
         return ingredientsInMeal.get(i);
     }
 
+    public void setCategory(MealCategory category) {
+        this.category = category;
+    }
 
+    public MealCategory getCategory() {
+        return this.category;
+    }
+
+    //Instantiate New Meal Items:
+
+    /**
+     * Adds a new ingredient into the meal.
+     * @param ingredient IngredientInStorage
+     */
+    public void addIngredient(IngredientInStorage ingredient) {
+        ingredientsInMeal.add(ingredient);
+    }
+
+    /**
+     * Removes an ingredient from a given meal.
+     * @param i int
+     */
+    public void deleteIngredient(int i) {
+        ingredientsInMeal.remove(i);
+    }
+
+    /**
+     * Adds a new recipe into the meal.
+     * @param recipe Recipe
+     */
+    public void addRecipe(Recipe recipe) {
+        recipesInMeal.add(recipe);
+    }
+
+    /**
+     * Removes a recipe from a given meal.
+     * @param i int
+     */
+    public void deleteRecipe(int i) {
+        recipesInMeal.remove(i);
+    }
 
 }
