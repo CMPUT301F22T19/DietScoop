d1904094c2ec0a601b2f23f3343b0db7cbaf04da
Albert Dinh <adinh.1867@gmail.com>
2022-11-16 18:27:13 -0700

    Added Recipe comparator by title

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Data/Comparators/RecipeComparator.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Data/Comparators/RecipeComparator.java
@@ -1,5 +1,18 @@
 package com.example.dietscoop.Data.Comparators;
 
+import com.example.dietscoop.Data.Recipe.Recipe;
+
+import java.util.Comparator;
+
 public class RecipeComparator {
+
+    public static class byRecipeTitle implements Comparator<Recipe> {
+
+        @Override
+        public int compare(Recipe t1, Recipe t2) {
+            return t1.getDescription().compareTo(t2.getDescription());
+        }
+    }
+
 
 }
