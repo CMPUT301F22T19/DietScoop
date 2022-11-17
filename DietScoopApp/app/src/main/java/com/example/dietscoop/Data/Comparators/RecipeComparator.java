a8d39ebb621fd80583293d9b56eb63d7685f4439
Albert Dinh <adinh.1867@gmail.com>
2022-11-16 18:36:16 -0700

    Sorted by recipe prep time

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Data/Comparators/RecipeComparator.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Data/Comparators/RecipeComparator.java
@@ -1,18 +1,31 @@
 package com.example.dietscoop.Data.Comparators;
 
 import com.example.dietscoop.Data.Recipe.Recipe;
-
+import java.lang.Integer;
 import java.util.Comparator;
 
 public class RecipeComparator {
 
+    /**
+     * This will order recipes by title
+     */
     public static class byRecipeTitle implements Comparator<Recipe> {
-
         @Override
         public int compare(Recipe t1, Recipe t2) {
             return t1.getDescription().compareTo(t2.getDescription());
         }
     }
+
+    /**
+     * This orders the Recipes by prep time
+     */
+    public static class byRecipePrepTime implements  Comparator<Recipe> {
+        @Override
+        public int compare(Recipe t1, Recipe t2) {
+            return Integer.compare(t1.getPrepTime(), t2.getPrepTime());
+        }
+    }
+
 
 
 }
