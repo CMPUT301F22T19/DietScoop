621541e46a92998d3e4a637b2bacc7d08a5f5ba0
Arjun <arjun.mehta1001@gmail.com>
2022-11-22 13:10:41 -0700

    setup started

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Activities/ShoppingListActivity.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Activities/ShoppingListActivity.java
@@ -4,21 +4,33 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dietscoop.Data.Ingredient.Ingredient;
 import com.example.dietscoop.Data.Ingredient.IngredientInRecipe;
 import com.example.dietscoop.Database.IngredientStorage;
 import com.example.dietscoop.R;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.util.ArrayList;
+
 public class ShoppingListActivity extends NavigationActivity {
     RecyclerView shoppingListView;
-
+//    TODO: populate neededIngredients using some comparison
+    ArrayList<Ingredient> neededIngredients;
+    TextView descriptionSort, categorySort;
 
     ActionBar topBar;
+
+    public enum sortSelection {
+        DESCRIPTION,
+        CATEGORY
+    }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,7 +38,15 @@ public class ShoppingListActivity extends NavigationActivity {
         setContentView(R.layout.activity_shopping_list);
 
         initNavigationActivity();
+        navBar.setSelectedItemId(R.id.shopping);
         setUpActionBar();
+
+        shoppingListView = findViewById(R.id.shopping_list);
+        shoppingListView.setLayoutManager(new LinearLayoutManager(this));
+
+
+        descriptionSort = findViewById(R.id.description_text);
+        categorySort = findViewById(R.id.category_text);
 
     }
 
