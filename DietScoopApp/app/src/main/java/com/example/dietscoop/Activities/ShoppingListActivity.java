443b25cad044383ad4c3ea94f3b47b04365156aa
Arjun <arjun.mehta1001@gmail.com>
2022-11-25 10:51:25 -0700

    recycler view setup with test val

--- DietScoopApp/app/src/main/java/com/example/dietscoop/Activities/ShoppingListActivity.java
+++ DietScoopApp/app/src/main/java/com/example/dietscoop/Activities/ShoppingListActivity.java
@@ -11,8 +11,11 @@ import androidx.appcompat.app.ActionBar;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.dietscoop.Adapters.IngredientRecipeAdapter;
 import com.example.dietscoop.Data.Ingredient.Ingredient;
+import com.example.dietscoop.Data.Ingredient.IngredientCategory;
 import com.example.dietscoop.Data.Ingredient.IngredientInRecipe;
+import com.example.dietscoop.Data.Ingredient.IngredientUnit;
 import com.example.dietscoop.Database.IngredientStorage;
 import com.example.dietscoop.R;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,7 +25,8 @@ import java.util.ArrayList;
 public class ShoppingListActivity extends NavigationActivity {
     RecyclerView shoppingListView;
 //    TODO: populate neededIngredients using some comparison
-    ArrayList<Ingredient> neededIngredients;
+    ArrayList<IngredientInRecipe> neededIngredients;
+    IngredientRecipeAdapter ingListAdapter;
     TextView descriptionSort, categorySort;
 
     ActionBar topBar;
@@ -35,7 +39,8 @@ public class ShoppingListActivity extends NavigationActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        neededIngredients =  new ArrayList<Ingredient>();
+        neededIngredients =  new ArrayList<IngredientInRecipe>();
+        neededIngredients.add(new IngredientInRecipe("gabagool", IngredientUnit.g, 3, IngredientCategory.Meat));
 
         setContentView(R.layout.activity_shopping_list);
 
@@ -44,8 +49,12 @@ public class ShoppingListActivity extends NavigationActivity {
         setUpActionBar();
 
         shoppingListView = findViewById(R.id.shopping_list);
+        shoppingListView.setHasFixedSize(false);
         shoppingListView.setLayoutManager(new LinearLayoutManager(this));
 
+        ingListAdapter = new IngredientRecipeAdapter(this, neededIngredients);
+
+        shoppingListView.setAdapter(ingListAdapter);
 
         descriptionSort = findViewById(R.id.description_text);
         categorySort = findViewById(R.id.category_text);
